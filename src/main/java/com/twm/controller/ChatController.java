d8467e17cbc9e312b615a74c6dc871a4758cfaf6
Jackchiang1010 <jackgg44109@gmail.com>
2024-08-24 16:33:16 +0800

    [feat]Concatenation chatGPT API

--- src/main/java/com/twm/controller/ChatController.java
+++ src/main/java/com/twm/controller/ChatController.java
@@ -1,14 +1,25 @@
 package com.twm.controller;
 
 import com.twm.service.chat.ChatService;
-import com.twm.service.chat.impl.ChatServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 public class ChatController {
 
     private final ChatService chatService;
+
+    @RequestMapping(value = "/chat", method = RequestMethod.POST)
+    public ResponseEntity<?> chat(@RequestParam("msg") String prompt) {
+        Map<String, Object> response = chatService.chat(prompt);
+        return ResponseEntity.ok(response);
+    }
 
 }
