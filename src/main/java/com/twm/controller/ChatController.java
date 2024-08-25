7a0718caef724c82d4ca457acf36f9a8d152f34c
Sean <shane60704@gmail.com>
2024-08-25 15:52:50 +0800

    [feat] integrate the AI Response API with a chat page

--- src/main/java/com/twm/controller/ChatController.java
+++ src/main/java/com/twm/controller/ChatController.java
@@ -1,12 +1,12 @@
 package com.twm.controller;
 
+import com.twm.dto.error.ChatRequest;
 import com.twm.service.chat.ChatService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -14,11 +14,14 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class ChatController {
 
+    private static final Logger log = LoggerFactory.getLogger(ChatController.class);
     private final ChatService chatService;
 
-    @RequestMapping(value = "/chat", method = RequestMethod.POST)
-    public ResponseEntity<?> chat(@RequestParam("msg") String prompt) {
-        Map<String, Object> response = chatService.chat(prompt);
+    @PostMapping("/chat")
+    public ResponseEntity<?> chat(@RequestBody ChatRequest chatRequest) {
+        log.info(chatRequest.toString());
+        Map<String, Object> response = chatService.chat(chatRequest.getUserMessage());
+        log.info(response.toString());
         return ResponseEntity.ok(response);
     }
 
