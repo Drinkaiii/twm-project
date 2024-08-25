bb4f1c531ce50b847d50170eb869198618e0d146
Sean <shane60704@gmail.com>
2024-08-25 23:22:20 +0800

    [feat] build native sign up API

--- src/main/java/com/twm/repository/user/UserRepository.java
+++ src/main/java/com/twm/repository/user/UserRepository.java
@@ -1,6 +1,9 @@
 package com.twm.repository.user;
 
+import com.twm.dto.UserDto;
+
 public interface UserRepository {
-
-
+    Integer createNativeUser(UserDto userDto);
+    UserDto getNativeUserByEmailAndProvider(String email);
+    UserDto getUserById(int userId);
 }
--- src/main/java/com/twm/service/user/UserService.java
+++ src/main/java/com/twm/service/user/UserService.java
@@ -1,4 +1,9 @@
 package com.twm.service.user;
 
+import com.twm.dto.UserDto;
+
+import java.util.Map;
+
 public interface UserService {
+    Map<String, Object> signUp(UserDto userDto);
 }
