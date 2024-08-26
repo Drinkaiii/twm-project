45cddfc88ca7f38557502f69b7acab874ac49bad
Sean <shane60704@gmail.com>
2024-08-26 18:01:48 +0800

    [feat] complete the integration of the existing information API

--- src/main/resources/static/assets/js/chat.js
+++ src/main/resources/static/assets/js/chat.js
@@ -3,16 +3,55 @@ document.addEventListener('DOMContentLoaded', () => {
     const messageInput = document.querySelector('.textareaBox textarea');
     const messageContainer = document.querySelector('.chat')
     const firstMessage = "您好，歡迎使用台灣大哥大智能客服，我是小麥，很高興為您服務!";
+    const float = document.querySelector(".float");
+    const float_content= document.createElement("div");
+    const prompt = document.createElement("p");
+    prompt.textContent = "小麥猜你想知道";
+    prompt.className = "prompt";
+    const button_container = document.createElement("div");
+    button_container.className = "button-container";
+    float_content.appendChild(prompt);
+    float_content.appendChild(button_container);
     let sessionId = null;
     addMessage(firstMessage,"chatgpt");
+    fetch('/api/1.0/chat/routines')
+        .then(response => { return response.json(); })
+        .then(data => {
+            const response = data.data;
+            response.forEach(item => {
+                console.log(`ID: ${item.id}, Type Name: ${item.type_name}`);
+                const button = document.createElement('button');
+                button.className = 'btn';
+                button.textContent = item.id+". "+item.type_name;
+                button.setAttribute('data-id', item.id);
+                button.addEventListener('click',function() {
+                    const id = this.getAttribute('data-id');
+                    floatBoxClose();
+                    faqInfo(id);
+                })
+                button_container.appendChild(button);
+                float.appendChild(float_content);
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            prompt.textContent = "系統異常，請點擊下方按鈕轉接至真人客服。"
+            const button = document.createElement('button');
+            button.className = 'btn';
+            button.textContent = "前往真人客服頁面";
+            button.addEventListener('click', function() {
+                window.location.href = '#';
+            });
+            button_container.appendChild(button);
+            float.appendChild(float_content);
+        })
+
     messageInput.addEventListener('input', () => {
         if (messageInput.value.trim().length > 0){
             floatBoxClose();
         }
     });
-    $(".float .btn").on("click", function () {
-        floatBoxClose();
-    });
+
     sendButton.addEventListener('click', function(event) {
         event.preventDefault();
         const userMessage = messageInput.value;
@@ -41,7 +80,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 addMessage(response,"chatgpt");
             })
             .catch(error => {
+                sessionId = data.sessionId;
                 console.error('Error:', error);
+                addMessage("系統出現問題，請稍後再嘗試使用。","chatgpt");
                 //loadingMessage.textContent = '很抱歉，系統維護中，將為您轉接至真人客服。';
             })
 
@@ -58,19 +99,45 @@ document.addEventListener('DOMContentLoaded', () => {
         dialog.className = "dialog";
         const content = document.createElement("div");
         content.className = "content";
-        content.textContent = text;
         const time = document.createElement("time");
         time.textContent = timeString;
-        dialog.appendChild(content);
-        dialog.appendChild(time);
         if(role === "user"){
+            content.textContent = text;
+            dialog.appendChild(content);
+            dialog.appendChild(time);
             li.className = "msg msgRight";
+            li.appendChild(dialog);
+            messageContainer.appendChild(li);
+        }else if(role === "chatgpt"){
+            content.textContent = text;
+            dialog.appendChild(content);
+            dialog.appendChild(time);
+            li.className = "msg";
+            const avatar = document.createElement("i");
+            avatar.className = "avatar";
+            li.appendChild(avatar);
             li.appendChild(dialog);
             messageContainer.appendChild(li);
         }else{
             li.className = "msg";
             const avatar = document.createElement("i");
             avatar.className = "avatar";
+            const guidance = document.createElement("p");
+            guidance.textContent = "請選擇以下選項：";
+            content.appendChild(guidance);
+            text.forEach(item => {
+                const linkButton = document.createElement("button");
+                linkButton.textContent = item.question;
+                linkButton.className = "btn";
+                linkButton.style.textAlign = "left";
+                linkButton.setAttribute('data-id', item.id);
+                linkButton.addEventListener("click",function (){
+                    const id = this.getAttribute('data-id');
+                    faqAnswer(id);
+                })
+                content.appendChild(linkButton);
+            })
+            dialog.appendChild(content);
             li.appendChild(avatar);
             li.appendChild(dialog);
             messageContainer.appendChild(li);
@@ -84,6 +151,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function floatBoxClose() {
         $(".float").slideUp(600);
+    }
+
+    function faqInfo(id){
+        fetch(`/api/1.0/chat/routines?type=${id}`)
+            .then(response => { return response.json(); })
+            .then(data => {
+                const response = data.data;
+                addMessage(response,"robot");
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            })
+    }
+    function faqAnswer(id){
+        fetch(`/api/1.0/chat/routines?question=${id}`)
+            .then(response => { return response.json(); })
+            .then(data => {
+                const response = data.data;
+                addMessage(response,"chatgpt");
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            })
     }
 
 });
