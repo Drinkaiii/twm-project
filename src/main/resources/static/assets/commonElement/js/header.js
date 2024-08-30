3c6ca8314e0f6c72c9cac5222cbab99020a2be10
Sean <shane60704@gmail.com>
2024-08-30 17:56:14 +0800

    [feat] add a logout feature and optimize the login mechanism

--- src/main/resources/static/assets/commonElement/js/header.js
+++ src/main/resources/static/assets/commonElement/js/header.js
@@ -5,3 +5,25 @@ function header() {
   $("body").prepend('<div class="overlay"></div>');
 
 }
+document.addEventListener('DOMContentLoaded', () => {
+  // user-info
+  document.querySelector('.icon-user').addEventListener('click', function() {
+    var infoWindow = document.querySelector('header .user .info-window');
+    if (infoWindow.style.display === 'block') {
+      infoWindow.style.display = 'none';
+    } else {
+      infoWindow.style.display = 'block';
+    }
+  });
+
+  // log-out button
+  document.querySelector('.log-out-content').addEventListener('click',function (){
+    localStorage.removeItem('jwtToken');
+    localStorage.removeItem('userId');
+    window.location.href = '/account_login.html';
+  });
+
+  document.querySelector('.user-info').textContent = `用戶 : `+localStorage.getItem("userInfo");
+})
+
+
