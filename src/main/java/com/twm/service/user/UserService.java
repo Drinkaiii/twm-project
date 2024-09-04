b360c1b484002490fc8a7163c311613197daf69a
wendy0705 <wendy9007054@gmail.com>
2024-09-04 23:18:20 +0800

    [fix] remove session and save captcha to db

--- src/main/java/com/twm/service/user/UserService.java
+++ src/main/java/com/twm/service/user/UserService.java
@@ -1,22 +1,29 @@
 package com.twm.service.user;
 
-import com.twm.dto.UserDto;
 import com.twm.dto.ResetPasswordDto;
+import com.twm.dto.UserDto;
 import com.twm.dto.supportDto;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.http.ResponseEntity;
 
 import java.util.Map;
 
 public interface UserService {
     Map<String, Object> signUp(UserDto userDto);
-    Map<String, Object> signIn(Map<String, Object> signInRequest,HttpSession session);
+
+    Map<String, Object> signIn(Map<String, Object> signInRequest, HttpSession session);
+
     void sendResetPasswordEmail(String email);
+
     Boolean resetPassword(ResetPasswordDto resetPasswordDto);
-    boolean validateCaptcha(String captchaInput, HttpSession session);
+
+    boolean validateCaptcha(String captchaId, String captcha);
+
     boolean validateEmail(String email);
+
     Boolean updateAuthTime(String userId);
+
     Map solveJwt(String token);
+
     Boolean saveSupportRequestRecord(supportDto supportDto);
 
 }
